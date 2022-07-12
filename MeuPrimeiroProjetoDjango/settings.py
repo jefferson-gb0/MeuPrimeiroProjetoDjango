2362c579f11aa53bb1c36afddee7a109a4bf97ab
jefferson.santos <jeffersongb0@gmail.com>
2022-07-11 21:28:12 -0300

    remove configuraçao do sqlite

--- MeuPrimeiroProjetoDjango/settings.py
+++ MeuPrimeiroProjetoDjango/settings.py
@@ -76,8 +76,6 @@ WSGI_APPLICATION = 'MeuPrimeiroProjetoDjango.wsgi.application'
 
 DATABASES = {
     'default': {
-        # 'ENGINE': 'django.db.backends.sqlite3',
-        # 'NAME': BASE_DIR / 'db.sqlite3',
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'bd_rh',
         'USER': 'root',
